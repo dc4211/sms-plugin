de8dea7656fcbbd4e611c1f20269e50046b86ae6
deepak <deepak1881.dc@gmail.com>
2022-05-26 17:45:02 +0530

    complete design

--- admin/partials/scheduled-sms-admin-display.php
+++ admin/partials/scheduled-sms-admin-display.php
@@ -183,124 +183,118 @@
          </div>
       </div>
       <!---Popup when delete Ends --->
-      
-	   <!---Popup when Add New --->
+      <!---Popup when Add New --->
       <div id="add" class="overlay">
          <div class="popup">
             <div class="S_sms_box">
                <h3>Add New Schedule</h3>
                <!--- Message Box Starts --->
-               <!--- Header Starts--->
-               <div class="S_sms_box_header">
+               <form action="#">
+                  <!--- Content Start --->
                   <input type="text" name="#" placeholder="Enter message title">
-               </div>
-               <!--- Header End--->
-               <!--- Content Start --->
-               <div class="S_sms_box_content">
-                  <div class="S_select_day_time">
-                     <div class="S_select_day">
+                  <div class="S_sms_box_content">
+                     <div class="S_select_day_time">
+                        <div class="S_select_day">
+                           <select>
+                              <option disabled selected>Select day</option>
+                              <option>Monday</option>
+                              <option>Tuesday</option>
+                              <option>Wednesday</option>
+                              <option>Thursday</option>
+                              <option>Friday</option>
+                              <option>Saturday</option>
+                           </select>
+                        </div>
+                        <div class="S_select_time">
+                           <input type="time" name="#">
+                        </div>
+                     </div>
+                     <div class="S_select_tag">
                         <select>
-                           <option disabled>Select day</option>
-                           <option>Monday</option>
-                           <option>Tuesday</option>
-                           <option>Wednesday</option>
-                           <option>Thursday</option>
-                           <option>Friday</option>
-                           <option>Saturday</option>
+                           <option disabled selected>Select tag</option>
+                           <option>enrolled_VCE_physics</option>
+                           <option>enrolled_VCE_maths</option>
+                           <option>enrolled_VCE_science</option>
+                           <option>enrolled_VCE_english</option>
+                           <option>enrolled_VCE_biology</option>
+                           <option>enrolled_VCE_french</option>
                         </select>
                      </div>
-                     <div class="S_select_time">
-                        <input type="time" name="#">
-                     </div>
+                     <textarea placeholder="Message Body"></textarea>
                   </div>
-                  <div class="S_select_tag">
-                     <select>
-                        <option disabled>Select tag</option>
-                        <option>enrolled_VCE_physics</option>
-                        <option>enrolled_VCE_maths</option>
-                        <option>enrolled_VCE_science</option>
-                        <option>enrolled_VCE_english</option>
-                        <option>enrolled_VCE_biology</option>
-                        <option>enrolled_VCE_french</option>
-                     </select>
+                  <!--- Content End --->
+                  <!--- Footer Start --->
+                  <div class="S_sms_box_footer">
+                     <button class="S_sms_box_add" href="#">Add</button>
+                     <a class="S_sms_box_delete" href="#">Close</a>
                   </div>
-                  <textarea placeholder="Message Body"></textarea>
-               </div>
-               <!--- Content End --->
-               <!--- Footer Start --->
-               <div class="S_sms_box_footer">
-                  <button class="S_sms_box_add" href="#">Add</button>
-                  <a class="S_sms_box_delete" href="#">Close</a>
-               </div>
-               <!--- Footer End --->
+                  <!--- Footer End --->
+               </form>
             </div>
             <!--- Message Box End --->
          </div>
       </div>
       <!---Popup when Add New Ends --->	
-	   
-	   
-	   
-	  <!---Popup When Edit --->
+      <!---Popup When Edit --->
       <div id="edit" class="overlay">
          <div class="popup">
             <div class="S_sms_box">
                <h3>Edit Schedule</h3>
                <!--- Message Box Starts --->
                <!--- Content Start --->
-			  <input type="text" name="#" value="Text Message Name">
-
-               <div class="S_sms_box_content">
-                  <div class="S_select_day_time">
-                     <div class="S_select_day">
+               <form action="#">
+                  <input type="text" name="#" value="Text Message Name">
+                  <div class="S_sms_box_content">
+                     <div class="S_select_day_time">
+                        <div class="S_select_day">
+                           <select>
+                              <option selected>Monday</option>
+                              <option>Tuesday</option>
+                              <option>Wednesday</option>
+                              <option>Thursday</option>
+                              <option>Friday</option>
+                              <option>Saturday</option>
+                           </select>
+                        </div>
+                        <div class="S_select_time">
+                           <input type="time" name="#">
+                        </div>
+                     </div>
+                     <div class="S_select_tag">
                         <select>
-                           <option selected>Monday</option>
-                           <option>Tuesday</option>
-                           <option>Wednesday</option>
-                           <option>Thursday</option>
-                           <option>Friday</option>
-                           <option>Saturday</option>
+                           <option selected>enrolled_VCE_physics</option>
+                           <option>enrolled_VCE_maths</option>
+                           <option>enrolled_VCE_science</option>
+                           <option>enrolled_VCE_english</option>
+                           <option>enrolled_VCE_biology</option>
+                           <option>enrolled_VCE_french</option>
                         </select>
                      </div>
-                     <div class="S_select_time">
-                        <input type="time" name="#">
-                     </div>
+                     <textarea placeholder="Message Body">A reminder that your class starts at 6pm today. The link to attand is https://google.com</textarea>
                   </div>
-                  <div class="S_select_tag">
-                     <select>
-                        <option selected>enrolled_VCE_physics</option>
-                        <option>enrolled_VCE_maths</option>
-                        <option>enrolled_VCE_science</option>
-                        <option>enrolled_VCE_english</option>
-                        <option>enrolled_VCE_biology</option>
-                        <option>enrolled_VCE_french</option>
-                     </select>
+                  <!--- Content End --->
+                  <!--- Footer Start --->
+                  <div class="S_sms_box_footer">
+                     <button class="S_sms_box_add" href="#">Save</button>
+                     <a class="S_sms_box_delete" href="#">Close</a>
                   </div>
-                  <textarea placeholder="Message Body">A reminder that your class starts at 6pm today. The link to attand is https://google.com</textarea>
-               </div>
-               <!--- Content End --->
-               <!--- Footer Start --->
-               <div class="S_sms_box_footer">
-                  <button class="S_sms_box_add" href="#">Save</button>
-                  <a class="S_sms_box_delete" href="#">Close</a>
-               </div>
-               <!--- Footer End --->
+                  <!--- Footer End --->
+               </form>
             </div>
             <!--- Message Box End --->
          </div>
       </div>
       <!---Popup when Edit Ends --->	  
-	   
    </div>
 </div>
 <script>
    jQuery('.S_sms_box_pause').click(function() {
        jQuery('.S_sms_box_content').toggleClass('pause-overlay');
    })	
-	
-jQuery(".S_sms_box_pause").click(function() {
+   
+   jQuery(".S_sms_box_pause").click(function() {
         	jQuery('.S_sms_box_pause').html(jQuery('.S_sms_box_pause').text() == 'Pause' ? 'Resume' : 'Pause');
-
-});	
-	
+   
+   });	
+   
 </script>
